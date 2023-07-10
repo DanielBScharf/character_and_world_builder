c387c065bc956c2071417a43107def35c4b82843
Daniel Scharf <id.scharfd@gmail.com>
2023-07-10 13:16:29 -0700

    worked on characters controller

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,2 +1,25 @@
 class CharactersController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show, :new]
+
+  def index
+    @characters = Characters.all
+  end
+
+  def show
+    @character = Character.find[params[:id]]
+  end
+
+  def new
+    @character = Character.new
+  end
+
+  def create
+
+  end
+
+  private
+  def character_params
+    params.require(:character).permit(:name, :description, :age, :attributes, :background)
+
+  end
 end
