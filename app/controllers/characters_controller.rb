8a7ffc7d87bb9c9655588a8ae8246bf32e04337f
Daniel Scharf <id.scharfd@gmail.com>
2023-07-13 16:16:57 -0700

    did the workings for open ai api. worked on the views

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -12,17 +12,47 @@ class CharactersController < ApplicationController
   def new
     @character = Character.new
 
-    prompt = "Respond in JSON form and include no other commentary, JSON object should be as follows {'name': '', 'description': '', 'background': '' [] } . Give me the a name name, description, and background for a #{character_params}."
-    OpenaiService.new(prompt).call
+    prompt = "Respond in JSON form and include no other commentary, JSON object should be as follows {'name': '', 'description': '', 'background': '' [] } . Give me the a name name, description, and background for a #{character_params.age} year old #{character_params.nationlity} person."
+    response = OpenaiService.new(prompt).call
+    @name = response.name
+    @nationlity = character_params.nationlity
+    @age = character_params.age
+    @description = response.age
+    @background = response.background
+    @character.name = @name
+    @character.age = @age
+    @character.nationlity = @nationlity
+    @character.description = @description
+    @character.background = @background
   end
 
   def create
+    @character = Character.new
+
+    prompt = "Respond in JSON form and include no other commentary, JSON object should be as follows {'name': '', 'description': '', 'background': '' [] } . Give me the a name name, description, and background for a #{character_params.age} year old #{character_params.nationlity} person."
+    response = OpenaiService.new(prompt).call
+    @name = response.name
+    @nationlity = character_params.nationlity
+    @age = character_params.age
+    @description = response.age
+    @background = response.background
+    @character.name = @name
+    @character.age = @age
+    @character.nationlity = @nationlity
+    @character.description = @description
+    @character.background = @background
+
+    if @character.save
+      redirect_to character_path(@character)
+    else
+      render :new, status: :unprocessable_entity
+    end
 
   end
 
   private
   def character_params
-    params.require(:character).permit(:name, :age, :nationlity)
+    params.require(:character).permit(:age, :nationlity)
 
   end
 end
